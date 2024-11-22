56adf3126016316e3e8915159f86b11e9976ca07
xoti$ <xoticdollarsign@outlook.com>
2024-11-22 17:07:50 +0300

    реализован Swagger

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -16,6 +16,20 @@ import (
 	"github.com/xoticdsign/auf-citaty-api/logging"
 )
 
+// List all quotes
+//
+//	@description	Возвращает полный список цитат, хранящихся в базе данных. Полезно для получения всех доступных данных для анализа, отображения или других операций. Цитаты возвращаются в формате JSON.
+//
+//	@id				list-all
+//	@tags			Операции с цитатами
+//
+//	@summary		Предоставляет все цитаты
+//	@produce		json
+//	@security		KeyAuth
+//	@success		200	{object}	database.Quote
+//	@failure		401	{object}	errorhandler.Error	Происходит,	если	не			был				предоставлен	ключ	API
+//	@failure		500	{object}	errorhandler.Error	Происходит,	если	произошла	неопределенная	ошибка
+//	@router			/ [get]
 func ListAll(c *fiber.Ctx) error {
 	logging.Logger.Info(
 		"Запрос получен",
@@ -34,6 +48,20 @@ func ListAll(c *fiber.Ctx) error {
 	return c.JSON(quotes)
 }
 
+// Random quote
+//
+//	@description	Возвращает случайную цитату из базы данных. Если цитата отсутствует в кэше, то она извлекается из базы данных, добавляется в кэш и возвращается пользователю. Позволяет отображать динамическое содержимое, не перегружая базу данных. Случайность обеспечивается генератором случайных чисел.
+//
+//	@id				random-quote
+//	@tags			Операции с цитатами
+//
+//	@summary		Предоставляет случайную цитату
+//	@produce		json
+//	@security		KeyAuth
+//	@success		200	{object}	database.Quote
+//	@failure		401	{object}	errorhandler.Error	Происходит,	если	не			был				предоставлен	ключ	API
+//	@failure		500	{object}	errorhandler.Error	Происходит,	если	произошла	неопределенная	ошибка
+//	@router			/random [get]
 func RandomQuote(c *fiber.Ctx) error {
 	logging.Logger.Info(
 		"Запрос получен",
@@ -73,6 +101,22 @@ func RandomQuote(c *fiber.Ctx) error {
 	})
 }
 
+// Quote by ID
+//
+//	@description	Возвращает цитату по её уникальному идентификатору (ID). Если цитата не найдена в кэше, происходит обращение к базе данных. Полученная цитата затем сохраняется в кэш для ускорения последующих запросов. Если запрошенного ID нет в базе данных, возвращается ошибка.
+//
+//	@id				quote-id
+//	@tags			Операции с цитатами
+//
+//	@summary		Предоставляет цитату по заданному ID
+//	@produce		json
+//	@param			id	path	string	false	"Позволяет указать ID цитаты"	example(105)
+//	@security		KeyAuth
+//	@success		200	{object}	database.Quote
+//	@failure		401	{object}	errorhandler.Error	Происходит,	если	не				был				предоставлен	ключ	API
+//	@failure		404	{object}	errorhandler.Error	Происходит,	если	запрашиваемой	цитаты			не				существует
+//	@failure		500	{object}	errorhandler.Error	Происходит,	если	произошла		неопределенная	ошибка
+//	@router			/{id} [get]
 func QuoteID(c *fiber.Ctx) error {
 	logging.Logger.Info(
 		"Запроc получен",
--- routes/routes.go
+++ routes/routes.go
@@ -2,11 +2,14 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 
+	_ "github.com/xoticdsign/auf-citaty-api/docs"
 	"github.com/xoticdsign/auf-citaty-api/handlers"
 )
 
 func GetRoutes(api *fiber.App) {
+	api.Get("/swagger/*", swagger.HandlerDefault)
 	api.Get("/", handlers.ListAll)
 	api.Get("/random", handlers.RandomQuote)
 	api.Get("/:id", handlers.QuoteID)
